90480ab9c7e97920fdbde9a11c9f9e0e28ca49c6
Черных Александр <razor01478920@mail.ru>
2025-06-25 22:52:01 +0300

    Сузил тип для E, скорректировал наименование существующих методов, добавил новых

--- bastion/src/commonMain/kotlin/io/ravenzip/bastion/result/Result.kt
+++ bastion/src/commonMain/kotlin/io/ravenzip/bastion/result/Result.kt
@@ -2,47 +2,62 @@ package io.ravenzip.bastion.result
 
 import io.ravenzip.bastion.verdict.Verdict
 
-sealed interface Result<out T, out W, out E> {
+sealed interface Result<out T, out W, out E : Throwable> {
     data class Success<out T>(val data: T) : Result<T, Nothing, Nothing>
 
     data class Warned<out T, out W>(val data: T, val warning: W) : Result<T, W, Nothing>
 
-    class Error<out E>(val error: E) : Result<Nothing, Nothing, E>
+    class Error<out E : Throwable>(val error: E) : Result<Nothing, Nothing, E>
 }
 
-fun <T, W, E> Result<T, W, E>.toVerdict(): Verdict<T, E> =
+fun <T, W, E : Throwable> Result<T, W, E>.toVerdict(): Verdict<T, E> =
     when (this) {
         is Result.Success -> Verdict.Success(data)
         is Result.Warned -> Verdict.Success(data)
         is Result.Error -> Verdict.Error(error)
     }
 
-val <T, W, E> Result<T, W, E>.isSuccess: Boolean
+val <T, W, E : Throwable> Result<T, W, E>.isSuccess: Boolean
     get() = this is Result.Success
 
-val <T, W, E> Result<T, W, E>.isWarned: Boolean
+val <T, W, E : Throwable> Result<T, W, E>.isWarned: Boolean
     get() = this is Result.Warned
 
-val <T, W, E> Result<T, W, E>.isError: Boolean
+val <T, W, E : Throwable> Result<T, W, E>.notIsError: Boolean
+    get() = this !is Result.Error
+
+val <T, W, E : Throwable> Result<T, W, E>.isError: Boolean
     get() = this is Result.Error
 
-fun <T, W, E> Result<T, W, E>.dataOrNull(): T? =
+fun <T, W, E : Throwable> Result<T, W, E>.dataOrThrow(): T =
+    when (this) {
+        is Result.Success -> data
+        is Result.Warned -> data
+        is Result.Error -> throw error
+    }
+
+fun <T, W, E : Throwable> Result<T, W, E>.dataOrNull(): T? =
     when (this) {
         is Result.Success -> data
         is Result.Warned -> data
         is Result.Error -> null
     }
 
-fun <T, W, E> Result<T, W, E>.dataOrElse(defaultValue: () -> T): T =
+fun <T, W, E : Throwable> Result<T, W, E>.dataOrDefault(default: () -> T): T =
     when (this) {
         is Result.Success -> data
         is Result.Warned -> data
-        is Result.Error -> defaultValue()
+        is Result.Error -> default()
     }
 
-fun <T, W, E> Result<T, W, E>.warningOrNull(): W? = if (this is Result.Warned) warning else null
+fun <T, W, E : Throwable> Result<T, W, E>.warningOrNull(): W? =
+    if (this is Result.Warned) warning else null
 
-fun <T, W, E> Result<T, W, E>.errorOrNull(): E? = if (this is Result.Error) error else null
+fun <T, W, E : Throwable> Result<T, W, E>.errorOrNull(): E? =
+    if (this is Result.Error) error else null
 
-fun <T, W, E> Result<T, W, E>.errorOrElse(defaultError: () -> E): E =
+fun <T, W, E : Throwable> Result<T, W, E>.errorOrDefault(defaultError: () -> E): E =
     if (this is Result.Error) error else defaultError()
+
+fun <T, W, E : Throwable> Result<T, W, E>.throwIfError(): Result<T, W, E> =
+    if (this is Result.Error) throw error else this
--- bastion/src/commonMain/kotlin/io/ravenzip/bastion/verdict/Verdict.kt
+++ bastion/src/commonMain/kotlin/io/ravenzip/bastion/verdict/Verdict.kt
@@ -2,35 +2,46 @@ package io.ravenzip.bastion.verdict
 
 import io.ravenzip.bastion.result.Result
 
-sealed interface Verdict<out T, out E> {
+sealed interface Verdict<out T, out E : Throwable> {
     data class Success<out T>(val data: T) : Verdict<T, Nothing>
 
-    data class Error<out E>(val error: E) : Verdict<Nothing, E>
+    data class Error<out E : Throwable>(val error: E) : Verdict<Nothing, E>
 }
 
-val <T, E> Verdict<T, E>.isSuccess: Boolean
-    get() = this is Verdict.Success
-
-val <T, E> Verdict<T, E>.isError: Boolean
-    get() = this is Verdict.Error
-
-fun <T, E> Verdict<T, E>.dataOrNull(): T? =
-    when (this) {
-        is Verdict.Success -> data
-        is Verdict.Error -> null
-    }
-
-fun <T, E> Verdict<T, E>.dataOrElse(defaultValue: () -> T): T =
-    when (this) {
-        is Verdict.Success -> data
-        is Verdict.Error -> defaultValue()
-    }
-
-fun <T, E> Verdict<T, E>.toResult() =
+fun <T, E : Throwable> Verdict<T, E>.toResult() =
     when (this) {
         is Verdict.Success -> Result.Success(data)
         is Verdict.Error -> Result.Error(error)
     }
 
-fun <T, E> Verdict<T, E>.errorOrElse(defaultError: () -> E): E =
-    if (this is Verdict.Error) error else defaultError()
+val <T, E : Throwable> Verdict<T, E>.isSuccess: Boolean
+    get() = this is Verdict.Success
+
+val <T, E : Throwable> Verdict<T, E>.isError: Boolean
+    get() = this is Verdict.Error
+
+fun <T, E : Throwable> Verdict<T, E>.dataOrThrow(): T? =
+    when (this) {
+        is Verdict.Success -> data
+        is Verdict.Error -> throw error
+    }
+
+fun <T, E : Throwable> Verdict<T, E>.dataOrNull(): T? =
+    when (this) {
+        is Verdict.Success -> data
+        is Verdict.Error -> null
+    }
+
+fun <T, E : Throwable> Verdict<T, E>.dataOrDefault(defaultValue: () -> T): T =
+    when (this) {
+        is Verdict.Success -> data
+        is Verdict.Error -> defaultValue()
+    }
+
+fun <T, E : Throwable> Verdict<T, E>.errorOrNull(): E? = if (this is Verdict.Error) error else null
+
+fun <T, E : Throwable> Verdict<T, E>.errorDefault(default: () -> E): E =
+    if (this is Verdict.Error) error else default()
+
+fun <T, E : Throwable> Verdict<T, E>.throwIfError(): Verdict<T, E> =
+    if (this is Verdict.Error) throw error else this
